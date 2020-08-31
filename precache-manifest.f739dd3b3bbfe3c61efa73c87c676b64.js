73f9532945d12797e76c30bad6386ba8f58eb97e
Nguyen Trong Phat <phatnt@rakumo.vn>
2020-08-31 16:11:46 +0700

    Updates

---
+++ precache-manifest.f739dd3b3bbfe3c61efa73c87c676b64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e634b365442fc9cbea65e45461351c6",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "17d89e808c97ef14a406",
+    "url": "/demo/static/css/2.d84865de.chunk.css"
+  },
+  {
+    "revision": "e1134f7b8b3b5db66360",
+    "url": "/demo/static/css/main.39b5ce7d.chunk.css"
+  },
+  {
+    "revision": "17d89e808c97ef14a406",
+    "url": "/demo/static/js/2.d2912e05.chunk.js"
+  },
+  {
+    "revision": "876e403427d7be82fb4e8ac24123aaf3",
+    "url": "/demo/static/js/2.d2912e05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1134f7b8b3b5db66360",
+    "url": "/demo/static/js/main.5cbdc50b.chunk.js"
+  },
+  {
+    "revision": "30c197e06ef64cd06220",
+    "url": "/demo/static/js/runtime-main.50c93e66.js"
+  }
+]);
