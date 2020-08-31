c6998613080ba93fa98ff1aa67cd51f61af80611
Nguyen Trong Phat <phatnt@rakumo.vn>
2020-08-31 16:54:21 +0700

    Updates

---
+++ precache-manifest.a373051c31563f9dbbe9ca1d48ead620.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beb1ada82014ee7de88dba9b01b67e3c",
+    "url": "/BusinesscardScannerPWA_Draft/index.html"
+  },
+  {
+    "revision": "1253fda1ac036ec852f7",
+    "url": "/BusinesscardScannerPWA_Draft/static/css/2.d84865de.chunk.css"
+  },
+  {
+    "revision": "b7fb501b771279a610c4",
+    "url": "/BusinesscardScannerPWA_Draft/static/css/main.39b5ce7d.chunk.css"
+  },
+  {
+    "revision": "1253fda1ac036ec852f7",
+    "url": "/BusinesscardScannerPWA_Draft/static/js/2.0915fc92.chunk.js"
+  },
+  {
+    "revision": "876e403427d7be82fb4e8ac24123aaf3",
+    "url": "/BusinesscardScannerPWA_Draft/static/js/2.0915fc92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7fb501b771279a610c4",
+    "url": "/BusinesscardScannerPWA_Draft/static/js/main.df087cff.chunk.js"
+  },
+  {
+    "revision": "36c8bddad7ea1f1f290a",
+    "url": "/BusinesscardScannerPWA_Draft/static/js/runtime-main.4cca7ce5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/git@github.com:JackTheCode/BusinesscardScannerPWA_Draft.git/precache-manifest.365c91d2c221b2755f42a86052bd8c18.js"
+  "/BusinesscardScannerPWA_Draft/precache-manifest.a373051c31563f9dbbe9ca1d48ead620.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/git@github.com:JackTheCode/BusinesscardScannerPWA_Draft.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/BusinesscardScannerPWA_Draft/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
